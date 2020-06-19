8e8d36d89a4f6c6fe194cfd210c321c4a034da42
abid <shaikh.abidali@gmail.com>
2020-06-19 14:32:15 +0530

    Display Search

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,7 +6,7 @@ class Stock < ApplicationRecord
       secret_token: 'Tsk_8cf6ec8f5ecb4e3993b714f90fa29249',
       endpoint: 'https://sandbox.iexapis.com/v1')
 
-    client.price(ticker_symbol)
-  end
+    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+   end
 
 end

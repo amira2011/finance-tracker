56a099f507e49edfd53dee53cc3751a1e7eee44a
unknown <shaikh.abidali@gmail.com>
2020-06-19 09:14:52 +0530

    Updated Stock

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,12 @@
 class Stock < ApplicationRecord
+
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(
+      publishable_token: 'Tpk_42731396d5fe4af5a5e5cdce8b83aab3',
+      secret_token: 'Tsk_8cf6ec8f5ecb4e3993b714f90fa29249',
+      endpoint: 'https://sandbox.iexapis.com/v1')
+
+    client.price(ticker_symbol)
+  end
+
 end

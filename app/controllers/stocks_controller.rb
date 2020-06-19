5c69560952b193ff1f36e8bc73f53db6ee7b1223
unknown <shaikh.abidali@gmail.com>
2020-06-19 21:26:56 +0530

    AJAX

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,7 +3,10 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock= Stock.new_lookup(params[:stock])
         if @stock
-        render 'users/my_portfolio'
+          respond_to do |format|
+            format.js {render partial: 'users/result'}
+          end
+        #render 'users/my_portfolio'
         else
           flash[:alert]="Please Enter Valid Symbol to Search"
           redirect_to my_portfolio_path

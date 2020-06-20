204299e01947f8c5140003a190e73ebe83e86ea4
abid <shaikh.abidali@gmail.com>
2020-06-20 15:15:53 +0530

    NavBar

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :user_stocks
   devise_for :users
   get 'welcome/index'
-  root 'users#my_portfolio'
+  root 'welcome#index'
   get 'my_portfolio' , to: 'users#my_portfolio'
   get 'search_stock' ,to: 'stocks#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
